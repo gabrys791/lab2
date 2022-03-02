a09d6be5680702c3bfff021cf29d54c07fcc95c3
gabrys791 <100271821+gabrys791@users.noreply.github.com>
2022-03-02 10:35:16 +0100

    lab2

---
+++ main.py
@@ -0,0 +1,76 @@
+#a=5
+#b=6
+#if a>b:
+    #print('a jest większe')
+#elif a<b:
+    #print('b jest wieksze')
+#else:
+    #print('liczby sa rowne')
+# a= input('wczytaj liczbe: ')
+# print(a)
+# print(type(a))
+# a=int(a)
+# print(type(a))
+# a=input('wczytaj 1 liczbe: ')
+# b=input('wczytaj 2 liczbe: ')
+# c=input('wczytaj 3 liczbe: ')
+# d=input('wczytaj 4 liczbe: ')
+# a=int(a)
+# b=int(b)
+# c=int(c)
+# d=int(d)
+# if (a>b) & (c>d):
+#     print('liczba a jest wieksza od b i liczba c jest większa od liczby d')
+# else:
+#     print('liczba a jest mniejsza od liczby b lub liczba c jest mniejsza od liczby d')
+# for a in range(0,7,1):
+#     print(a)
+# lista=['a',3,4,5.6]
+# for a in lista:
+#     print(a)
+# else:
+#     print('wyświetlone zostały elementy z listy')
+# licznik=0
+# while licznik !=11:
+#     print(licznik)
+#     licznik+=1
+# lista=[4,6,2,5,3,9,8,7]
+# a=input('Podaj liczbę: ')
+# licznika=0
+# while licznika!=len(lista):
+#     if int(a)-lista[licznika]==0:
+#         print('liczba '+ str(a) + ' - ' + str(lista[licznika])+ ' = 0')
+#         break
+#     else:
+#         licznika+=1
+# else:
+#     print('żadna z wartości nie dała odpwiedniego wyniku')
+# lista1=[4,6,3,2,7,9,5,1]
+# lista2=[4,7,5,3]
+# suma=[]
+# for a in lista1:
+#     for b in lista2:
+#         wynik=a+b
+#         suma.append(wynik)
+#     print(suma)
+# a=input('1 liczba: ')
+# b=input('2 liczba: ')
+# try:
+#     a=int(a)
+#     b=int(b)
+#     wynik=a / b
+#     print(wynik)
+# except ZeroDivisionError:
+#     print('nie dziel cholero przez zero')
+# except ValueError:
+#     print('nie wczytano liczby całkowitej')
+napisy=['fajny','program','pyton']
+słownik={'c':1,'+':2,'+':3,'tez':4,'fajny':5}
+słownik['python']=6
+słownik['cos']=10
+print(słownik)
+print(napisy)
+
+
+
+
